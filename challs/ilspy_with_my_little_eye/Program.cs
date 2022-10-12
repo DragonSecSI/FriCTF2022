d4f7c6ae75b6729e0bc2d78fa8e9ecaca3ca1b8d
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-10-13 00:33:13 +0200

    ILSpy

---
+++ challs/ilspy_with_my_little_eye/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+string iv = "t0ju/tgPQ3n2g56QY9xWaw==";
+string key = "rRWl2x6LVsCIvvXd17zdRyYigj1TmQ/kICwGm1spYuc=";
+string flagEnc = "lB9Y3bF6f4T/gQ2eHfCZaxlRMSuDO+8FOcjI4TdvQf9KutxRzzVOfuQuyn4KglO/kBaGEsnacNGMlNskQP1oGQ==";
+
+Console.WriteLine("What is the flag?");
+var inp = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(inp))
+{
+    Console.WriteLine("Please give me a string!");
+    return;
+}
+
+var bytes = Encoding.UTF8.GetBytes(inp);
+using var aes = Aes.Create();
+aes.IV = Convert.FromBase64String(iv);
+aes.Key = Convert.FromBase64String(key);
+bytes = aes.EncryptCbc(bytes, aes.IV);
+
+if (Convert.FromBase64String(flagEnc).SequenceEqual(bytes)) 
+    Console.WriteLine("You got it!");
+else
+    Console.WriteLine("Try again...");
